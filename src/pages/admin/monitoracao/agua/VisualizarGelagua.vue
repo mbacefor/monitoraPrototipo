c894b3ea384cc4af9740c8e6ceb3d8e35a120cc9
Marcelo <marcelo.alcantara@serpro.gov.br>
2023-11-11 23:35:45 -0300

    Correção de navegação

--- src/pages/admin/monitoracao/agua/VisualizarGelagua.vue
+++ src/pages/admin/monitoracao/agua/VisualizarGelagua.vue
@@ -6,11 +6,11 @@
         <va-popover
           icon="water_drop"
           color="warning"
-          message="Clique e salve os dados do Botijão!"
+          message="Clique para voltar para a tela de listagem!"
           placement="right"
           open
         >
-          <va-chip shadow color="primary" to="Listar">{{ t('monitora.garrafao.botaoSalvar') }}</va-chip>
+          <va-chip shadow color="primary" to="listagem">{{ t('monitora.garrafao.listagem') }}</va-chip>
         </va-popover>
       </va-card-content>
     </va-card>
@@ -28,16 +28,16 @@
         <form>
           <div class="grid grid-cols-12 gap-6">
             <div class="flex md:col-span-4 sm:col-span-6 col-span-12">
-              <va-input v-model="dtoGarrafao._id" placeholder="Identificador do garrafão" label="identificador" />
+              <va-input v-model="dtoGarrafao._id" placeholder="Identificador do garrafão" label="identificador" readonly/>
             </div>
             <div class="col-span-full md:col-span-4 grid grid-cols-1 md:grid-cols-3">
               <fieldset class="flex flex-col">
-                <va-switch v-model="dtoGarrafao.ativo" label="Ativo" class="mr-8 mb-2" />
+                <va-switch v-model="dtoGarrafao.ativo" label="Ativo" class="mr-8 mb-2" readonly/>
               </fieldset>
             </div>
             <div class="flex md:col-span-4 sm:col-span-6 col-span-12"></div>
             <div class="flex md:col-span-4 sm:col-span-6 col-span-12">
-              <va-input v-model="dtoGarrafao.nome" placeholder="Texto identificador Garrafão" label="Nome" />
+              <va-input v-model="dtoGarrafao.nome" placeholder="Texto identificador Garrafão" label="Nome" readonly />
             </div>
             <div class="flex md:col-span-8 sm:col-span-6 col-span-12">
               <va-input
@@ -45,6 +45,7 @@
                 type="textarea"
                 placeholder="Campo livre para descrição do botijão"
                 label="Descrição"
+                readonly
               >
               </va-input>
             </div>
@@ -57,13 +58,14 @@
                 track-by="_id"
                 value-by="_id"
                 :options="dtoListaDeviceID"
+                readonly
               />
             </div>
             <div class="flex md:col-span-4 sm:col-span-6 col-span-12">
-              <va-input v-model="dtoGarrafao.pesoMinimo" type="number" label="Peso Mínimo"> </va-input>
+              <va-input v-model="dtoGarrafao.pesoMinimo" type="number" label="Peso Mínimo" readonly> </va-input>
             </div>
             <div class="flex md:col-span-4 sm:col-span-6 col-span-12">
-              <va-input v-model="dtoGarrafao.pesoMaximo" type="number" label="Peso Máximo"> </va-input>
+              <va-input v-model="dtoGarrafao.pesoMaximo" type="number" label="Peso Máximo" readonly> </va-input>
             </div>
           </div>
         </form>
